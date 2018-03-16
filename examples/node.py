dce74759e777cb83f483e6270c376d15c23b0236
Poorwa Hirve <poorwa@Poorwas-MacBook-Air.local>
2018-03-15 18:44:34 -0600

    My edits

--- examples/node.py
+++ examples/node.py
@@ -6,32 +6,109 @@ import random
 import raftos
 
 
-async def run(node_id):
-    # Any replicated object with get/set functions
-    data_id = raftos.Replicated(name='data_id')
+class FTStack():
+    
+    async def sCreate(self, label):
+        stack_id = random.randint(1, 10000)
+        self.data_list = raftos.ReplicatedList(name=str(stack_id)+'data_list')
+        self.label_dict = raftos.ReplicatedDict(name=str(stack_id)+'label_dict')
+        return stack_id
 
-    # Dict-like object: data.update(), data['key'] etc
-    data = raftos.ReplicatedDict(name='data')
+    async def sId(self, label):
+        return await self.label_dict[label]
 
-    # List-like object: data_list.append(), data_list[-1]
-    data_list = raftos.ReplicatedList(name='data_list')
-
-    while True:
-        # We can also check if raftos.get_leader() == node_id
-        await raftos.wait_until_leader(node_id)
-
-        await asyncio.sleep(2)
-
-        current_id = random.randint(1, 1000)
+    async def sPush(self, stack_id, item):
         data_map = {
-            str(current_id): {
-                'created_at': datetime.now().strftime('%d/%m/%y %H:%M')
-            }
+            stack_id : item
         }
+            
+        await self.data_list.append(data_map)
 
-        await data_id.set(current_id)
-        await data.update(data_map)
-        await data_list.append(data_map)
+    async def sPop(self, stack_id):
+        length = await self.data_list.length()
+        for i in range(length-1, -1, -1):
+            dict_item = await self.data_list[i]
+            
+            if stack_id in dict_item.keys():
+                item = dict_item.pop(stack_id)
+                return item
+
+    async def sTop(self, stack_id):
+        length = await self.data_list.length()
+        
+        for i in range(length-1, -1, -1):
+            dict_item = await self.data_list[i]
+            
+            if stack_id in dict_item.keys():
+                return dict_item[stack_id]
+
+    async def sSize(self, stack_id):
+        length = 0
+        total_length = await self.data_list.length()
+        for i in range(total_length-1, -1, -1):
+            dict_item = await self.data_list[i]
+            
+            if stack_id in dict_item.keys():
+                length += 1
+        
+        return length
+
+    async def print_stack(self, stack_id):
+        length = await self.data_list.length()
+        
+        for i in range(length):
+            dict_item = await self.data_list[i]
+            
+            if stack_id in dict_item.keys():
+                print (dict_item[stack_id], end=' ')
+
+
+
+
+#data_dict = dict()
+#label_dict = dict()
+#stack_id = ""
+#label = 0
+#data_list = []
+ftstack = FTStack()
+async def run(node_id):
+
+    label = random.randint(1, 1000)
+    stack_id = await ftstack.sCreate(label)
+#    print (stack_id)
+    if stack_id != "":
+        await raftos.wait_until_leader(node_id)
+        await ftstack.label_dict.update({label : stack_id})
+        while True:
+            print (await ftstack.sId(label))
+
+            item = random.randint(1, 1000000)
+
+            print ("LENGTH BEFORE PUSH:", await ftstack.sSize(stack_id))
+
+            print ("PUSH:", item)
+
+            await ftstack.sPush(stack_id, item)
+
+            item = await ftstack.sTop(stack_id)
+
+            print ("TOP:", item)
+
+            length = await ftstack.sSize(stack_id)
+#
+            if length % 2 == 0:
+                print ("LENGTH AFTER PUSH / BEFORE POP:", await ftstack.sSize(stack_id))
+
+                item = await ftstack.sPop(stack_id)
+#
+                print ("POP", item)
+#
+                print ("LENGTH AFTER POP:", await ftstack.sSize(stack_id))
+#
+            await ftstack.print_stack(stack_id)
+            print ()
+            await asyncio.sleep(2)
+
 
 
 if __name__ == '__main__':
--- examples/run_cluster.sh
+++ examples/run_cluster.sh
@@ -7,6 +7,15 @@ rm -f *.storage
 rm -f *.state_machine
 
 # Start
-../env/bin/python node.py --node "8000" --cluster "8000 8001 8002" &
-../env/bin/python node.py --node "8001" --cluster "8000 8001 8002" &
-../env/bin/python node.py --node "8002" --cluster "8000 8001 8002" &
+python node.py --node "9000" --cluster "9000 9001 9002 9003 9004" &
+python node.py --node "9001" --cluster "9000 9001 9002 9003 9004" &
+python node.py --node "9002" --cluster "9000 9001 9002 9003 9004" &
+python node.py --node "9003" --cluster "9000 9001 9002 9003 9004" &
+python node.py --node "9004" --cluster "9000 9001 9002 9003 9004" &
+
+#python node.py --node "9012" --cluster "9012 9013 9014 9015 9016" &
+#python node.py --node "9013" --cluster "9012 9013 9014 9015 9016" &
+#python node.py --node "9014" --cluster "9012 9013 9014 9015 9016" &
+#python node.py --node "9015" --cluster "9012 9013 9014 9015 9016" &
+#python node.py --node "9016" --cluster "9012 9013 9014 9015 9016" &
+
